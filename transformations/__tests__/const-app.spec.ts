22566b74bc528a999466634dcc099476eca318a4
jia <Fire_Mars_j@163.com>
2021-07-17 16:34:57 +0800

    fix: const-app

--- transformations/__tests__/const-app.spec.ts
+++ transformations/__tests__/const-app.spec.ts
@@ -6,32 +6,44 @@ const transform = require('../const-app')
 defineInlineTest(
   transform,
   {},
-  `Vue.createApp(App).use(button_counter).use(router).use(store).mount("#app");
+  `import * as Vue from "vue";
+  Vue.createApp(App).use(button_counter).use(router).use(store).mount("#app");
 Vue.directive('demo', {})
 Vue.component('myComponent',{})
-
 `,
-  `const app = Vue.createApp(App).use(button_counter).use(router).use(store);
+  `import * as Vue from "vue";
+const app = Vue.createApp(App).use(button_counter).use(router).use(store);
+app.mount("#app");
 app.directive('demo', {})
-app.component('myComponent',{})
-
-app.mount("#app");`,
-
+app.component('myComponent',{})`,
   'add const app and transform Vue to app in main.js'
 )
 
 defineInlineTest(
   transform,
   {},
-  `const app = Vue.createApp(App).use(button_counter).use(router).use(store);
+  `import * as Vue from "vue";
+  const app = Vue.createApp(App).use(button_counter).use(router).use(store);
 Vue.directive('demo', {})
 Vue.component('myComponent',{})
-
 app.mount("#app");`,
-  `const app = Vue.createApp(App).use(button_counter).use(router).use(store);
+  `import * as Vue from "vue";
+  const app = Vue.createApp(App).use(button_counter).use(router).use(store);
 app.directive('demo', {})
 app.component('myComponent',{})
+app.mount("#app");`,
+  'do not add const app when app.mount() is already exist '
+)
 
+defineInlineTest(
+  transform,
+  {},
+  `import test from 'test'
+  Vue.prototype.shareConfig = shareConfig
+  Vue.createApp(App).mount("#app")`,
+  `import test from 'test'
+Vue.prototype.shareConfig = shareConfig
+const app = Vue.createApp(App);
 app.mount("#app");`,
 
   'do not add const app when app.mount() is already exist '
